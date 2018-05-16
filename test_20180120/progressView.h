5ae5992f2484e0ed480ca9f1ba743fe964de9a34
wangdewei <wangdewei@douyu.tv>
2018-05-16 19:54:20 +0800

    first commit

---
+++ test_20180120/progressView.h
@@ -0,0 +1,13 @@
+//
+//  progressView.h
+//  test_20180120
+//
+//  Created by wangdewei on 2018/1/20.
+//  Copyright © 2018年 wangdewei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface progressView : UIView
+
+@end
---
+++ test_20180120/progressView.m
@@ -0,0 +1,21 @@
+//
+//  progressView.m
+//  test_20180120
+//
+//  Created by wangdewei on 2018/1/20.
+//  Copyright © 2018年 wangdewei. All rights reserved.
+//
+
+#import "progressView.h"
+
+@implementation progressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
